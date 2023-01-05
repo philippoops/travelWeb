86751074dee72c980e4ed7a0f95cab63c1ff4364
Louis Philip Buenaflor <92981427+philippoops@users.noreply.github.com>
2023-01-05 08:20:55 +0800

    Add files via upload

---
+++ travel website/index.js
@@ -0,0 +1,14 @@
+let navbar = document.querySelector(".header .navbar");
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+// controls
+document
+  .querySelectorAll(".about .video-container .controls .control-btn")
+  .forEach((btn) => {
+    btn.onclick = () => {
+      let src = btn.getAttribute("data-src");
+      document.querySelector(".about .video-container .video").src = src;
+    };
+  });
